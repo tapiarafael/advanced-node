2b71d870412b7b86abe9ca50126d61baeaaf0415
Rafael Tapia <rafaelsilvatapia@gmail.com>
2023-12-02 17:38:21 -0300

    refactor: use jest mocks

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,34 +1,21 @@
-import type { LoadFacebookUserApi } from '@/data/contracts/apis'
 import { FacebookAuthenticationService } from '@/data/services'
 import { AuthenticationError } from '@/domain/errors'
 
-class LoadFacebookUserApiSpy implements LoadFacebookUserApi {
-  token?: string
-  callsCount = 0
-  result = undefined
-
-  async loadUser (params: LoadFacebookUserApi.Params): Promise<LoadFacebookUserApi.Result> {
-    this.callsCount++
-    this.token = params.token
-    return this.result
-  }
-}
-
 describe('FacebookAuthenticationService', () => {
   it('should call LoadFacebookUserApi with correct params', async () => {
-    const loadFacebookUserApi = new LoadFacebookUserApiSpy()
-    const sut = new FacebookAuthenticationService(loadFacebookUserApi)
+    const loadFacebookUserApiSpy = { loadUser: jest.fn() }
+    const sut = new FacebookAuthenticationService(loadFacebookUserApiSpy)
 
     await sut.execute({ token: 'valid-token' })
 
-    expect(loadFacebookUserApi.token).toBe('valid-token')
-    expect(loadFacebookUserApi.callsCount).toBe(1)
+    expect(loadFacebookUserApiSpy.loadUser).toHaveBeenCalledWith({ token: 'valid-token' })
+    expect(loadFacebookUserApiSpy.loadUser).toHaveBeenCalledTimes(1)
   })
 
   it('should throw AuthenticationError if token is expired or invalid', async () => {
-    const loadFacebookUserApi = new LoadFacebookUserApiSpy()
-    loadFacebookUserApi.result = undefined
-    const sut = new FacebookAuthenticationService(loadFacebookUserApi)
+    const loadFacebookUserApiSpy = { loadUser: jest.fn() }
+    loadFacebookUserApiSpy.loadUser.mockResolvedValueOnce(undefined)
+    const sut = new FacebookAuthenticationService(loadFacebookUserApiSpy)
 
     const authResult = await sut.execute({ token: 'invalid-token' })
 
