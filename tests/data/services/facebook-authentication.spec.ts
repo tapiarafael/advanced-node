f95d96603b2aa7d8e58561f3259222632ed6b4e1
Rafael Tapia <rafaelsilvatapia@gmail.com>
2023-12-03 20:33:35 -0300

    test: move sut to a factory method

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,14 +1,27 @@
-import { mock } from 'jest-mock-extended'
+import { mock, type MockProxy } from 'jest-mock-extended'
 
 import { FacebookAuthenticationService } from '@/data/services'
 import { AuthenticationError } from '@/domain/errors'
 import type { LoadFacebookUserApi } from '@/data/contracts/apis'
 
+type SutTypes = {
+  sut: FacebookAuthenticationService
+  loadFacebookUserApiSpy: MockProxy<LoadFacebookUserApi>
+}
+
+const makeSut = (): SutTypes => {
+  const loadFacebookUserApiSpy = mock<LoadFacebookUserApi>()
+  const sut = new FacebookAuthenticationService(loadFacebookUserApiSpy)
+
+  return {
+    sut,
+    loadFacebookUserApiSpy
+  }
+}
+
 describe('FacebookAuthenticationService', () => {
   it('should call LoadFacebookUserApi with correct params', async () => {
-    const loadFacebookUserApiSpy = mock<LoadFacebookUserApi>()
-    const sut = new FacebookAuthenticationService(loadFacebookUserApiSpy)
-
+    const { loadFacebookUserApiSpy, sut } = makeSut()
     await sut.execute({ token: 'valid-token' })
 
     expect(loadFacebookUserApiSpy.loadUser).toHaveBeenCalledWith({ token: 'valid-token' })
@@ -16,9 +29,8 @@ describe('FacebookAuthenticationService', () => {
   })
 
   it('should throw AuthenticationError if token is expired or invalid', async () => {
-    const loadFacebookUserApiSpy = mock<LoadFacebookUserApi>()
+    const { loadFacebookUserApiSpy, sut } = makeSut()
     loadFacebookUserApiSpy.loadUser.mockResolvedValueOnce(undefined)
-    const sut = new FacebookAuthenticationService(loadFacebookUserApiSpy)
 
     const authResult = await sut.execute({ token: 'invalid-token' })
 
